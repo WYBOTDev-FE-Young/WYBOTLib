fd3b18625de5fe6edc51ad1b49538cee2a2c7ab8
liyang <young_wxx@qq.com>
2026-01-21 17:30:43 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WYBOTLib",
+	platforms: [.iOS(.v15)],
+    products: [
+        .library(name: "WYBOTLib", targets: ["WYBOTLib"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-server-community/mqtt-nio", from: "2.12.1"),
+    ],
+    targets: [
+        .target(
+            name: "WYBOTLib",
+            path: "Sources",
+            dependencies: [
+                .product(name: "MQTTNIO", package: "mqtt-nio"),
+            ]
+        ),
+    ]
+    
+)
